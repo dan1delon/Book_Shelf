708a9a6e6ed8f60ff6a839eb3d70594ffca2518b
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-03-04 14:09:57 +0000

    Deploying to gh-pages from @ dan1delon/Book_Shelf@583a1d1a24b929589ca0969186c491e3da4ff7a0 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,22 +1,22 @@
-import{a as r}from"./assets/icons-579fda80.js";import"./assets/vendor-3d873957.js";const l="/Book_Shelf/assets/icons-a18bf9e2.svg#icon-ibooks",d="/Book_Shelf/assets/icons-a18bf9e2.svg#icon-trash";function h(){const o="selected-books",e=JSON.parse(localStorage.getItem(o))||[],n=document.getElementById("shopping-list-books-container");n.innerHTML="",e.forEach(t=>{const s=document.createElement("div");s.classList.add("book-card");const i=t.description?t.description:"We are sorry. There is no description for this book";s.innerHTML=`
+import{a as l}from"./assets/icons-579fda80.js";import"./assets/vendor-3d873957.js";const r="/Book_Shelf/assets/icons-a18bf9e2.svg#icon-ibooks",d="/Book_Shelf/assets/icons-a18bf9e2.svg#icon-trash";function h(){const o="selected-books",e=JSON.parse(localStorage.getItem(o))||[],n=document.getElementById("shopping-list-books-container");n.innerHTML="",e.forEach(t=>{const s=document.createElement("div");s.classList.add("book-card");const a=t.description?t.description:"We are sorry. There is no description for this book";s.innerHTML=`
       <div class="cover">
           <img src="${t.book_image}" alt="Book Cover" class="book-cover">
           <div class="about">
               <h2 class="book-title">${t.title}</h2>
               <p class="book-category">${t.list_name}</p>
-              <p class="book-description">${i}</p>
+              <p class="book-description">${a}</p>
               <p class="book-author">${t.author}</p>
           </div>
       </div>
       <div class="buy-book">
           <a href="${t.buy_links[0].url}" target="_blank">
-              <svg class="amazon" width="16" height="16">
-                  <use href="${r}"></use>
+              <svg class="amazon-link-img" width="16" height="16">
+                  <use href="${l}"></use>
               </svg>
           </a>
           <a href="${t.buy_links[1].url}" target="_blank">
-              <svg class="ibook" width="16" height="16">
-                  <use href="${l}"></use>
+              <svg class="apple-link-img" width="16" height="16">
+                  <use href="${r}"></use>
               </svg>
           </a>
       </div>
@@ -26,5 +26,5 @@ import{a as r}from"./assets/icons-579fda80.js";import"./assets/vendor-3d873957.j
                   <use href="${d}"></use>
               </svg>
           </div>
-      </button>`,n.appendChild(s)}),document.querySelectorAll(".btn-delete").forEach(t=>{t.addEventListener("click",function(){const s=this.getAttribute("data-book-id");p(s),this.closest(".book-card").remove(),c()})})}h();function p(o){const e="selected-books",a=(JSON.parse(localStorage.getItem(e))||[]).filter(t=>t._id!==o);localStorage.setItem(e,JSON.stringify(a))}function k(){return document.getElementById("shopping-list-books-container").children.length===0}function c(){const o=document.getElementById("cart-content"),e=document.getElementById("donation-block");k()?(o.style.display="block",e.style.display="none"):(o.style.display="none",e.style.display="block")}c();
+      </button>`,n.appendChild(s)}),document.querySelectorAll(".btn-delete").forEach(t=>{t.addEventListener("click",function(){const s=this.getAttribute("data-book-id");p(s),this.closest(".book-card").remove(),c()})})}h();function p(o){const e="selected-books",i=(JSON.parse(localStorage.getItem(e))||[]).filter(t=>t._id!==o);localStorage.setItem(e,JSON.stringify(i))}function g(){return document.getElementById("shopping-list-books-container").children.length===0}function c(){const o=document.getElementById("cart-content"),e=document.getElementById("donation-block");g()?(o.style.display="block",e.style.display="none"):(o.style.display="none",e.style.display="block")}c();
 //# sourceMappingURL=commonHelpers2.js.map
