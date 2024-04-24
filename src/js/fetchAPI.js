77a37296eb420a0dbf2bbdc06a1d7fe2c74ad081
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-04-24 13:23:57 +0300

    Update fetchAPI.js

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,21 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 
 export async function getCategories() {
-    const {data} = await axios.get('https://books-backend.p.goit.global/books/category-list');
-    return data;
+  const { data } = await axios.get(
+    'https://books-backend.p.goit.global/books/category-list'
+  );
+  return data;
 }
 
 export async function getTopBooks() {
-    const {data} = await axios.get('https://books-backend.p.goit.global/books/top-books');
-    return data;
+  const { data } = await axios.get(
+    'https://books-backend.p.goit.global/books/top-books'
+  );
+  return data;
 }
 
 export async function getBooksFromCategory(selectedCategory) {
-    const {data} = await axios.get(`https://books-backend.p.goit.global/books/category?category=${selectedCategory}`);
-    return data;
+  const { data } = await axios.get(
+    `https://books-backend.p.goit.global/books/category?category=${selectedCategory}`
+  );
+  return data;
 }
 
 export async function getBookById(bookId) {
-    const {data} = await axios.get(`https://books-backend.p.goit.global/books/${bookId}`);
-    return data;
+  const { data } = await axios.get(
+    `https://books-backend.p.goit.global/books/${bookId}`
+  );
+  return data;
 }
